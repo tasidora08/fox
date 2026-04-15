ee74c9b6667baa96ba44fcc9efbc98d34f0bd1aa
tasidora08 <tasi.dora08@gmail.com>
2026-04-15 11:59:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Jc() {
+    let jcO  = document.getElementById("jc").value;
+    fox.style.JustifyContent(jcO);
+}
+
+function Ai(){
+    let aiO  = document.getElementById("ai").value;
+
+}
